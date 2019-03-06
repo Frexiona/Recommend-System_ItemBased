55e6554800de1df7ca12562dc939b26c0e5ffe21
Frexiona <frexiona@foxmail.com>
2019-03-06 10:46:29 +0000

    Experiment 1

--- src/alg/ib/ExecuteIB_ML20M_1.java
+++ src/alg/ib/ExecuteIB_ML20M_1.java
@@ -1,11 +1,15 @@
 package alg.ib;
 
 import java.io.File;
+import java.util.Set;
 
 import alg.ib.neighbourhood.NearestNeighbourhood;
 import alg.ib.neighbourhood.Neighbourhood;
+import alg.ib.predictor.DeviationFromItemMeanPredictor;
+import alg.ib.predictor.NonPersonalisedPredictor;
 import alg.ib.predictor.Predictor;
 import alg.ib.predictor.SimpleAveragePredictor;
+import alg.ib.predictor.WeightedAveragePredictor;
 import similarity.metric.CosineMetric;
 import similarity.metric.PearsonMetric;
 import similarity.metric.SimilarityMetric;
@@ -15,45 +19,54 @@ import util.reader.DatasetReader;
 public class ExecuteIB_ML20M_1 {
 	public static void main(String[] args)
 	{
-		for (int k = 10; k <= 250; k += 10)
+		Object[] predictors = {new NonPersonalisedPredictor(), new SimpleAveragePredictor(), new WeightedAveragePredictor(), 
+				new DeviationFromItemMeanPredictor()};
+		String[] p_names = {"NonPersonalisedPredictor", "SimpleAveragePredictor", 
+				"WeightedAveragePredictor", "DeviationFromItemMeanPredictor"};
+		for (int i = 0; i< predictors.length; i++)
 		{
-			// configure the item-based CF algorithm - set the predictor, neighbourhood and similarity metric ...
-			Predictor predictor = new SimpleAveragePredictor();
-			Neighbourhood neighbourhood = new NearestNeighbourhood(k);
-			SimilarityMetric metric = new CosineMetric();
-			
-			// set the paths and filenames of the item file, genome scores file, train file and test file ...
-			String folder = "ml-20m-2018-2019";
-			String itemFile = folder + File.separator + "movies-sample.txt";
-			String itemGenomeScoresFile = folder + File.separator + "genome-scores-sample.txt";
-			String trainFile = folder + File.separator + "train.txt";
-			String testFile = folder + File.separator + "test.txt";	
-			
-			// set the path and filename of the output file ...
-			String outputFile = "results" + File.separator + "predictions.txt";
-			
-			////////////////////////////////////////////////
-			// Evaluates the CF algorithm (do not change!!):
-			// - the RMSE (if actual ratings are available) and coverage are output to screen
-			// - output file is created
-			DatasetReader reader = new DatasetReader(itemFile, itemGenomeScoresFile, trainFile, testFile);
-			ItemBasedCF ibcf = new ItemBasedCF(predictor, neighbourhood, metric, reader);
-			Evaluator eval = new Evaluator(ibcf, reader.getTestData());
-			
-			// Write to output file
-			eval.writeResults(outputFile);
-			
-			// Display which k what KNN used
-			System.out.printf("k = %d\n", k);
-			
-			// Display RMSE and coverage
-			Double RMSE = eval.getRMSE();
-			if(RMSE != null) System.out.printf("RMSE: %.6f\n", RMSE);
-			
-			double coverage = eval.getCoverage();
-			System.out.printf("coverage: %.2f%%\n", coverage);	
-			
-			System.out.println();
+			System.out.println(p_names[i] + "is running!!!");
+			for (int k = 10; k <= 250; k += 10)
+			{
+				// configure the item-based CF algorithm - set the predictor, neighbourhood and similarity metric ...
+				Predictor predictor = (Predictor) predictors[i];
+				Neighbourhood neighbourhood = new NearestNeighbourhood(k);
+				SimilarityMetric metric = new CosineMetric();
+				
+				// set the paths and filenames of the item file, genome scores file, train file and test file ...
+				String folder = "ml-20m-2018-2019";
+				String itemFile = folder + File.separator + "movies-sample.txt";
+				String itemGenomeScoresFile = folder + File.separator + "genome-scores-sample.txt";
+				String trainFile = folder + File.separator + "train.txt";
+				String testFile = folder + File.separator + "test.txt";	
+				
+				// set the path and filename of the output file ...
+				String outputFile = "results" + File.separator + "predictions.txt";
+				
+				////////////////////////////////////////////////
+				// Evaluates the CF algorithm (do not change!!):
+				// - the RMSE (if actual ratings are available) and coverage are output to screen
+				// - output file is created
+				DatasetReader reader = new DatasetReader(itemFile, itemGenomeScoresFile, trainFile, testFile);
+				ItemBasedCF ibcf = new ItemBasedCF(predictor, neighbourhood, metric, reader);
+				Evaluator eval = new Evaluator(ibcf, reader.getTestData());
+				
+				// Write to output file
+				eval.writeResults(outputFile);
+				
+				// Display which k what KNN used
+				System.out.printf("k = %d\n", k);
+				
+				// Display RMSE and coverage
+				Double RMSE = eval.getRMSE();
+				if(RMSE != null) System.out.printf("RMSE: %.6f\n", RMSE);
+				
+				double coverage = eval.getCoverage();
+				System.out.printf("coverage: %.2f%%\n", coverage);	
+				
+				System.out.println();
+		}
+		
 		}
 	}
 }
