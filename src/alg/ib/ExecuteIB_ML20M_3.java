f6cb3ef3023a552767676d0d2cba617469244560
Frexiona <frexiona@foxmail.com>
2019-03-06 10:50:19 +0000

    Experiment 3

--- src/alg/ib/ExecuteIB_ML20M_3.java
+++ src/alg/ib/ExecuteIB_ML20M_3.java
@@ -8,7 +8,9 @@ import alg.ib.predictor.DeviationFromItemMeanPredictor;
 import alg.ib.predictor.Predictor;
 import alg.ib.predictor.SimpleAveragePredictor;
 import similarity.metric.CosineMetric;
+import similarity.metric.MeanSquaredDifferenceMetric;
 import similarity.metric.PearsonMetric;
+import similarity.metric.PearsonSigWeightingMetric;
 import similarity.metric.SimilarityMetric;
 import util.evaluator.Evaluator;
 import util.reader.DatasetReader;
@@ -17,43 +19,53 @@ public class ExecuteIB_ML20M_3
 {
 	public static void main(String[] args)
 	{
-		// configure the item-based CF algorithm - set the predictor, neighbourhood and
-		// similarity metric ...
-		Predictor predictor = new DeviationFromItemMeanPredictor();
-		Neighbourhood neighbourhood = new NearestNeighbourhood(200);
-		SimilarityMetric metric = new CosineMetric();
+		
+		Object[] metrics = {new CosineMetric(), new PearsonMetric(), 
+				new PearsonSigWeightingMetric(50), new MeanSquaredDifferenceMetric()};
+		String[] m_names = {"CosineMetric", "PearsonMetric",
+				"PearsonSigWeightingMetric", "MeanSquaredDifferenceMetric"};
+		for (int i = 0; i < metrics.length; i++)
+		{
+			System.out.println(m_names[i] + "is running");
+			// configure the item-based CF algorithm - set the predictor, neighbourhood and
+			// similarity metric ...
+			Predictor predictor = new DeviationFromItemMeanPredictor();
+			Neighbourhood neighbourhood = new NearestNeighbourhood(200);
+			SimilarityMetric metric = (SimilarityMetric) metrics[i];
 
-		// set the paths and filenames of the item file, genome scores file, train file
-		// and test file ...
-		String folder = "ml-20m-2018-2019";
-		String itemFile = folder + File.separator + "movies-sample.txt";
-		String itemGenomeScoresFile = folder + File.separator + "genome-scores-sample.txt";
-		String trainFile = folder + File.separator + "train.txt";
-		String testFile = folder + File.separator + "test.txt";
+			// set the paths and filenames of the item file, genome scores file, train file
+			// and test file ...
+			String folder = "ml-20m-2018-2019";
+			String itemFile = folder + File.separator + "movies-sample.txt";
+			String itemGenomeScoresFile = folder + File.separator + "genome-scores-sample.txt";
+			String trainFile = folder + File.separator + "train.txt";
+			String testFile = folder + File.separator + "test.txt";
 
-		// set the path and filename of the output file ...
-		String outputFile = "results" + File.separator + "predictions.txt";
+			// set the path and filename of the output file ...
+			String outputFile = "results" + File.separator + "predictions.txt";
 
-		////////////////////////////////////////////////
-		// Evaluates the CF algorithm (do not change!!):
-		// - the RMSE (if actual ratings are available) and coverage are output to
-		//////////////////////////////////////////////// screen
-		// - output file is created
-		DatasetReader reader = new DatasetReader(itemFile, itemGenomeScoresFile, trainFile, testFile);
-		ItemBasedCF ibcf = new ItemBasedCF(predictor, neighbourhood, metric, reader);
-		Evaluator eval = new Evaluator(ibcf, reader.getTestData());
+			////////////////////////////////////////////////
+			// Evaluates the CF algorithm (do not change!!):
+			// - the RMSE (if actual ratings are available) and coverage are output to
+			//////////////////////////////////////////////// screen
+			// - output file is created
+			DatasetReader reader = new DatasetReader(itemFile, itemGenomeScoresFile, trainFile, testFile);
+			ItemBasedCF ibcf = new ItemBasedCF(predictor, neighbourhood, metric, reader);
+			Evaluator eval = new Evaluator(ibcf, reader.getTestData());
 
-		// Write to output file
-		eval.writeResults(outputFile);
+			// Write to output file
+			eval.writeResults(outputFile);
 
-		// Display RMSE and coverage
-		Double RMSE = eval.getRMSE();
-		if (RMSE != null)
-			System.out.printf("RMSE: %.6f\n", RMSE);
+			// Display RMSE and coverage
+			Double RMSE = eval.getRMSE();
+			if (RMSE != null)
+				System.out.printf("RMSE: %.6f\n", RMSE);
 
-		double coverage = eval.getCoverage();
-		System.out.printf("coverage: %.2f%%\n", coverage);
+			double coverage = eval.getCoverage();
+			System.out.printf("coverage: %.2f%%\n", coverage);
 
-		System.out.println();
+			System.out.println();
+		}
+		
 	}
 }
